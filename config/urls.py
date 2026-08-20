91f27994b4f7c074bd8e33ee9d38543579b7c47a
Aminata Diop <aminata.diop@ucad.edu.sn>
2026-08-20 19:55:47 +0000

    Redirect the site root to the catalogue
    The homepage had no route at all, returning a bare 404 on /.

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='catalogue:liste_produits')),
     path('admin/', admin.site.urls),
     path('catalogue/', include('catalogue.urls')),
     path('panier/', include('panier.urls')),
